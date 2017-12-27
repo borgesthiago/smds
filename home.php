4166334f8a9b6034d4b934b3e23ae396caff2ce5
Thiago Borges <thiagomat25@gmail.com>
2017-12-27 10:36:56 -0200

    projeto inicial versao

---
+++ home.php
@@ -0,0 +1,64 @@
+
+<!-- Marketing messaging and featurettes
+================================================== -->
+<!-- Wrap the rest of the page in another container to center all the content. -->
+<br />
+<div class="container marketing">
+	
+  <!-- Three columns of text below the carousel -->
+  <div class="row">
+	<div class="col-lg-4"><a href="sisdoc/adm/login.php" >
+	  <img  class="img-circle" src="imagens/logo-sisdoc.jpg" alt="Generic placeholder image" width="140" height="140"></a>
+	  
+	  <p>Sistema utilizado para dar entrada e saída em documentos (Ofício e Controle Interno).</p>
+	  <p></p>
+	</div><!-- /.col-lg-4 -->
+	
+	<div class="col-lg-4"><a href="patrimonio/adm/login.php">
+	  <img  src="imagens/logo-patrimonio.jpg" alt="Generic placeholder image" width="180" height="140"></a>
+	  
+	  <p>Sistema criado para controle de patrimônios.</p>
+	  <p></p>
+	</div><!-- /.col-lg-4 -->
+	
+	<div class="col-lg-4"><a href="planejamento/adm/login.php" >
+	  <img class="img-circle" src="imagens/logo-plan.jpg" alt="Generic placeholder image" width="140" height="140"></a>
+	  
+	  <p>Sistema para autorização de eventos em vias públicas.</p>
+	  <p>Em Construção</p>
+	</div><!-- /.col-lg-4 -->
+	
+	<div class="col-lg-4"><a href="dcit/adm/login.php">
+	  <img class="img-circle" src="imagens/logo-dcit.jpg" alt="Generic placeholder image" width="140" height="140"></a>
+	  
+	  <p>Sistema utilizado para cadastramento e controle de multas.</p>
+	  <p></p>
+	</div><!-- /.col-lg-4 -->
+	
+	<div class="col-lg-4"><a href="jari/adm/login.php">
+	  <img class="img-circle" src="imagens/logo-jari.jpg" alt="Generic placeholder image" width="140" height="140"></a>
+	 
+	  <p>Sistema utilizado para cadastramento e controle de Processos.</p>
+	  <p></p>
+	</div><!-- /.col-lg-4 -->
+  
+
+	<div class="col-lg-4"><a href="taxi/adm/login.php" >
+	  <img src="imagens/logo-taxi.jpg" alt="Generic placeholder image" width="140" height="140"></a>
+	  
+	  <p>Sistema utilizado para cadastramento e controle de Autorizatários.</p>
+	  <p></p>
+	</div><!-- /.col-lg-4 -->
+ </div><!-- /.row -->
+<br />
+<br />
+<center>
+Versão <a href="taxi/mobile/m.index.php">mobile</a></center>
+<br />
+  <!-- FOOTER -->
+  <footer>
+	<p class="pull-right"><a href="#">Back to top</a></p>
+	<p>&copy; 2016 Company, Inc. &middot; <a href="http://www.thsi.com.br" target="_blank">SistemaTHSI</a> </p>
+  </footer>
+
+</div><!-- /.container -->
